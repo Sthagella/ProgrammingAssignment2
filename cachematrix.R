b3bc03520ecce761f80906e84a0f6aee0e4e227d
Sthagella <srao20@gmail.com>
2017-02-26 12:22:52 -0500

    cachematrix.R and README.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This assignment is modeled after the exmaple for caching meahs that was provided
 
-## Write a short comment describing this function
+## This function stores the matrix provided by the user and solves for the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(inverse) m <<- inverse
+    getsolve <- function() m
+    list(set = set, get = get,
+    setsolve = setsolve,
+    getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function resturns the inverse of the matrix from the above if already available and if not solves for the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+## To run this function assign the makeCacheMatrix function to A and store its value in m. Call cacheSolve(m) to resolve for the inverse.
+## A <- makeCaheMatrix(matrix(1:4, 2, 2))
+## m <- A
+## cacheSolve(m)
+## The solve function looks for arguments solve(a, b, ...). 'a' can be a matrix in this case
+## and in the absence of 'b', b is taken to be an indentity matrix and solve returns inverse of a
+
